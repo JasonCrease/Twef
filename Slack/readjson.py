98c9aaac10d5cb5ccb91b2b2bd7680502d9ffcac
Jason Crease <jason.crease@red-gate.com>
2016-03-21 21:30:52 +0000

    rot13ish for anonymization

--- Slack/readjson.py
+++ Slack/readjson.py
@@ -37,12 +37,23 @@ for obj in fulldata:
     allMsgsDateTime.append(datetime.datetime.fromtimestamp(float(obj['ts'])))
     allMsgsDate.append(datetime.date.fromtimestamp(float(obj['ts'])))
 
+def rot13(s):
+    chars = "GHI78WXYZ0123ABCDEFJKLMNOPQRSTUV4569"
+    trans = chars[18:] + chars[:18]
+    rot_char = lambda c: trans[chars.find(c)] if chars.find(c)>-1 else c
+    return ''.join( rot_char(c) for c in s )      
+    
+for i in range(0, len(allMsgsUser) - 1):
+    allMsgsUser[i] = rot13(allMsgsUser[i])
+    
 def findHere(strToLook):
     return strToLook.find("<!here") >= 0 
 def findChannel(strToLook):
     return strToLook.find("<!channel") >= 0 
 def findAtX(strToLook):
     return strToLook.find("<@") >= 0 
+
+
 
 df = pd.DataFrame({ 'Time': allMsgsTime, 'Text' : allMsgsText, 'User' : allMsgsUser, 'DateTime': allMsgsDateTime , 'Date' : allMsgsDate, 'Reactions' : allMsgsReactions })
 df['AtHere']    = df['Text'].apply(findHere)
